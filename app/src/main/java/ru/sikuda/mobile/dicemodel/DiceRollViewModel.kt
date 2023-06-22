ee38730065e511bdfe09ec8ed9d67be2ea0b8ec0
ya.sikuda <ya.sikuda@yandex.ru>
2023-06-22 17:53:43 +0300

    add saveStateHandle

--- app/src/main/java/ru/sikuda/mobile/dicemodel/DiceRollViewModel.kt
+++ app/src/main/java/ru/sikuda/mobile/dicemodel/DiceRollViewModel.kt
@@ -1,27 +1,38 @@
 package ru.sikuda.mobile.dicemodel
 
-import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.io.Serializable
 import kotlin.random.Random
 
 data class DiceUiState(
     val firstDieValue: Int? = null,
     val secondDieValue: Int? = null,
     val numberOfRolls: Int = 0,
-)
+): Serializable
 
-class DiceRollViewModel : ViewModel() {
+class DiceRollViewModel(
+    val saveStateHandle: SavedStateHandle
+) : ViewModel() {
 
+    companion object {
+        const val strKeyStote = "dices"
+    }
+
+
+    //val uiState = saveStateHandle.getStateFlow(strKeyStote, DiceUiState())
     // Expose screen UI state
     private val _uiState = MutableStateFlow(DiceUiState())
     val uiState: StateFlow<DiceUiState> = _uiState.asStateFlow()
 
-    var uiData = mutableStateOf(DiceUiState())
-        private set
+//    var uiData = mutableStateOf(
+//        saveStateHandle.get<DiceUiState>(strKeyStote) ?: DiceUiState()
+//    )
+//        private set
 
 
     // Handle business logic
@@ -33,11 +44,14 @@ class DiceRollViewModel : ViewModel() {
                 numberOfRolls = currentState.numberOfRolls + 1,
             )
         }
-        uiData.value = DiceUiState(
-            firstDieValue = Random.nextInt(from = 1, until = 7),
-            secondDieValue = Random.nextInt(from = 1, until = 7),
-            numberOfRolls = uiData.value.numberOfRolls + 1,
-        )
+//        val newValue = DiceUiState(
+//            firstDieValue = Random.nextInt(from = 1, until = 7),
+//            secondDieValue = Random.nextInt(from = 1, until = 7),
+//            //numberOfRolls = uiData.value.numberOfRolls + 1,
+//            numberOfRolls = uiState.value.numberOfRolls + 1,
+//        )
+//        //uiData.value = newValue
+//        saveStateHandle.set(strKeyStote, newValue)
 
     }
 }
